8cffae240a90f94c9c76cf81a87ce77d34bfbe9e
KoZZZtik-3d <kv.sobolev06@gmail.com>
2024-12-03 19:35:27 +0300

    chore: InMemoryTaskManager deleteTask(int id) теперь удаляет этот id еще и из истории

--- src/tasktracker/taskmanager/InMemoryTaskManager.java
+++ src/tasktracker/taskmanager/InMemoryTaskManager.java
@@ -69,6 +69,7 @@ public class InMemoryTaskManager implements TaskManager {
     @Override
     public void deleteTask(int id) {
         tasks.remove(id);
+        historyManager.removeFromHistory(id);
     }
 
     @Override
