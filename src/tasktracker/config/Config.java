2aef3bbcd6b83f6378127924f68c59d3ecdaefab
KoZZZtik-3d <kv.sobolev06@gmail.com>
2024-12-03 19:22:25 +0300

    chore: в Config HISTORY_MAX_LEN изменено на 5, также добавлено invalidFileMessage

--- src/tasktracker/config/Config.java
+++ src/tasktracker/config/Config.java
@@ -3,12 +3,13 @@ package tasktracker.config;
 public class Config {
 
     public static final int DESCRIPTION_LENGHT_TO_PRINT = 20;
-    public static final int HISTORY_MAX_LEN = 3;
+    public static final int HISTORY_MAX_LEN = 5;
     public static final String DATA_FILE_NAME = "data.csv";
 //    public static final String DATA_FILE_PATH = "D:\\Games\\Project programma\\untitled5\\src" +
 //            "\\tasktracker\\resources\\data.csv";
     public static final String DATA_FILE_PATH = "D:\\Games\\Project programma\\untitled5\\" +
         "TaskTracker22455suoer\\src\\tasktracker\\resources\\data";
 
-
+    //Exceptions
+    public static final String invalidFileMessage = "Файл не удовлетворяет стандарту программы";
 }
