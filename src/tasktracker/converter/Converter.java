6e5336cdecca2eb13fb61eeaf32cb26ab35d4288
KoZZZtik-3d <kv.sobolev06@gmail.com>
2024-12-08 20:03:34 +0300

    chore: создан пустой класс Converter (видимо, на будущее)

---
+++ src/tasktracker/converter/Converter.java
@@ -0,0 +1,12 @@
+package tasktracker.converter;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import tasktracker.model.Task;
+
+import java.io.IOException;
+
+public class Converter {
+
+}
